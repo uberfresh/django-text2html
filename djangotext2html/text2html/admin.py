a7723f229fe12af7e478247fd8e93aa472f57a66
Batuhan <99hanbatu@windowslive.com>
2021-08-20 03:11:24 +0300

    text2html

---
+++ djangotext2html/text2html/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Editor
+# Register your models here.
+admin.site.register(Editor)
---
+++ djangotext2html/text2html/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+# Create your models here.
+
+class Editor(models.Model):
+	body = RichTextField(blank=True,null=True)
+
+	
